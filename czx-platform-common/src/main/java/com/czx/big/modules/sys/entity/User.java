6abb4244d1b429dea5d6e8ba99d8ad8e321bd598
huisetiankongj <421310742@qq.com>
2017-05-11 17:34:24 +0800

    s

--- czx-platform-common/src/main/java/com/czx/big/modules/sys/entity/User.java
+++ czx-platform-common/src/main/java/com/czx/big/modules/sys/entity/User.java
@@ -1,5 +1,12 @@
 package com.czx.big.modules.sys.entity;
 
-public class User {
+import com.czx.big.common.persistence.BaseEntity;
+
+public class User extends BaseEntity<User>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 }
